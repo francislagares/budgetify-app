56c3d5eee2d7be258b66e6d23c2cd64bb1796e5d
Francis Lagares <francis.lagares@gmail.com>
2020-05-19 17:42:13 +0200

    feat: add event listeners and read input data

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,46 @@
  ****************************************/
 const budgetController = (function () {})();
 
-const UIController = (function () {})();
+const UIController = (function () {
+  let DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
 
-const controller = (function (budgetCtrl, UICtrl) {})(
-  budgetController,
-  UIController
-);
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    }
+  };
+})();
+
+const controller = (function (budgetCtrl, UICtrl) {
+  let DOM = UICtrl.getDOMstrings();
+
+  const ctrlAddItem = () => {
+    // 1. Get the filled input data
+    let input = UICtrl.getInput();
+    console.log(input);
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  };
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', (event) => {
+    if (event.KeyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, UIController);
