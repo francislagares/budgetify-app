72255a1d7864bbe2760970170ea8062a3b02d015
Francis Lagares <francis.lagares@gmail.com>
2020-05-20 13:54:52 +0200

    feat: add item to UI controller

--- src/js/app.js
+++ src/js/app.js
@@ -55,7 +55,9 @@ const UIController = (() => {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return {
@@ -66,7 +68,41 @@ const UIController = (() => {
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+    addListItem: (obj, type) => {
+      let html, element;
+      // Create HTML template literal with placeholder variables
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+        html = `<div class="item clearfix" id="income-${obj.id}">
+                  <div class="item__description">${obj.description}</div>
+                  <div class="right clearfix">
+                    <div class="item__value">+ ${obj.value}</div>
+                    <div class="item__delete">
+                      <button class="item__delete--btn">
+                        <i class="ion-ios-close-outline"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>`;
+      } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+        html = `<div class="item clearfix" id="expense-${obj.id}">
+                  <div class="item__description">${obj.description}</div>
+                  <div class="right clearfix">
+                    <div class="item__value">- ${obj.value}</div>
+                    <div class="item__percentage">21%</div>
+                    <div class="item__delete">
+                      <button class="item__delete--btn">
+                        <i class="ion-ios-close-outline"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>`;
+      }
 
+      // Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', html);
+    },
     getDOMstrings: () => {
       return DOMstrings;
     }
@@ -91,6 +127,7 @@ const controller = ((budgetCtrl, UICtrl) => {
     // 2. Add the item to the budget controller
     let item = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
+    UICtrl.addListItem(item, input.type);
     // 4. Calculate the budget
     // 5. Display the budget on the UI
   };
