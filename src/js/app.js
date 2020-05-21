1df171a0812722b77d6f785a8d612fe9d3354761
Francis Lagares <francis.lagares@gmail.com>
2020-05-21 15:04:25 +0200

    feat: display budget to UI

--- src/js/app.js
+++ src/js/app.js
@@ -94,7 +94,11 @@ const UIController = (() => {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   return {
@@ -156,6 +160,17 @@ const UIController = (() => {
       fieldsArr[0].focus();
     },
 
+    displayBudget: (obj) => {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+    },
+
     getDOMstrings: () => {
       return DOMstrings;
     }
@@ -180,7 +195,7 @@ const controller = ((budgetCtrl, UICtrl) => {
     // 2. Return the budget
     let budget = budgetCtrl.getBudget();
     // 3. Display budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   // This is the main control center function of the application
@@ -205,7 +220,13 @@ const controller = ((budgetCtrl, UICtrl) => {
 
   return {
     init: () => {
-      return setupEventListeners();
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
+      setupEventListeners();
     }
   };
 })(budgetController, UIController);
